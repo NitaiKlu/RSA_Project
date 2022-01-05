6f773483d62c138fb39b1e88dcd78fdf6b7eeb93
nitaiklu <nitai.kluger@gmail.com>
2022-01-06 01:56:13 +0200

    tries

--- RSA_try.py
+++ RSA_try.py
@@ -8,14 +8,54 @@ p = 11
 N = p*q
 e = 3
 
+def gcdInverseCalc(a, b):
+    b0 = b
+    y = 0
+    inverse = 1
+ 
+    if (b == 1):
+        return 0
+ 
+    while (a > 1):
+        q = a/b
+        p = b 
+        #euclides algorithm 
+        b = a % b
+        a = p
+        p = y
+        #updating x and y
+        y = inverse - q * y
+        inverse = p
+
+    if( x < 0):
+        x = x + b0           
+    return x
+     
+#computes d: the inverse of num within Z_N => e * d = N*k + 1
+def Inverse(num, N):
+    gcd = numpy.gcd(num, N)
+    if(gcd != 1):
+        return 0
+    return gcdInverseCalc(num, N)
+    
+
 #computes: base to the power of power (mod N)
 def toPowerMod(base, power, N):
-    packet = pow(base,small_power) % N
-    power_ups = power / small_power
+    new_base = pow(base, small_power, N)
+    if(power % small_power == 0):
+        additional = 1
+    else:
+        additional = pow(base, power % small_power, N)
+    new_power = power / small_power
+    return toPowerMod(new_base, new_power, N) * additional
+   
+    
 
 #encrypts the message given
-def encrypt(encrypt):
+def encrypt(message, e, N):
+    return toPowerMod(message, e, N)
 
 
 #decrypts the message given
-def decrypt(decrypt):
+def decrypt(message, d, N):
+    return toPowerMod(message, d, N)
