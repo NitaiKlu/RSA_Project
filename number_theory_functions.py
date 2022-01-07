b846ede47744fa5466c7e3df79ad14751b36537b
giladgafny1 <giladgafny@campus.technion.ac.il>
2022-01-07 11:12:37 +0200

    1

---
+++ number_theory_functions.py
@@ -0,0 +1,146 @@
+from random import randrange
+import numpy
+import math
+
+from numpy import long
+
+small_power = 2
+def extended_gcd(a,b):
+    """
+    Returns the extended gcd of a and b
+
+    Parameters
+    ----------
+    a : Input data.
+    b : Input data.
+    Returns
+    -------
+    (d, x, y): d = gcd(a,b) = a*x + b*y
+    """
+    if (a<b):
+        return extended_gcd(b,a)
+    if(b==0):
+        return (a, 1, 0)
+    (gcd, x, y) = extended_gcd(a%b, b)
+    #gcd = 1, x = 1, y = -2, a = 3, b = 1
+    x1 = y #-2
+    y1 = x - y * (a//b) #7
+    return (gcd, x1, y1)
+
+
+
+
+def modular_inverse(a,n):
+    """
+    Returns the inverse of a modulo n if one exists
+
+    Parameters
+    ----------
+    a : Input data.
+    n : Input data.
+
+    Returns
+    -------
+    x: such that (a*x % n) == 1 and 0 <= x < n if one exists, else None
+    """
+    (gcd, inverse, y) = extended_gcd(a,n)
+    if(gcd != 1):
+        return 0
+    return inverse%n
+
+def modular_exponent(a, d, n):
+    """
+    Returns a to the power of d modulo n
+
+    Parameters
+    ----------
+    a : The exponential's base.
+    d : The exponential's exponent.
+    n : The exponential's modulus.
+
+    Returns
+    -------
+    b: such that b == (a**d) % n
+    """
+    base = long(a)
+    power = long(d)
+    new_base = pow(base, small_power, n)
+    if (power % small_power == 0):
+        additional = 1
+    else:
+        additional = pow(base, power % small_power, n)
+    new_power = power // small_power
+    if(new_power == 1):
+        return (pow(new_base, 1, n) * additional)%n
+    return (modular_exponent(new_base, new_power, n) * additional) %n
+
+def miller_rabin(n):
+    """
+    Checks the primality of n using the Miller-Rabin test
+
+    Parameters
+    ----------
+    n : The number to check
+
+    Returns
+    -------
+    b: If n is prime, b is guaranteed to be True.
+    If n is not a prime, b has a 3/4 chance at least to be False
+    """
+    a = randrange(1,n)
+    k = 0
+    d = n-1
+    while d % 2 == 0:
+        k = k + 1
+        d = d // 2
+    x = modular_exponent(a, d, n)
+    if x == 1 or x == n-1:
+        return True
+    for _ in range(k):
+        x = (x * x) % n
+        if x == 1:
+            return False
+        if x == n-1:
+            return True
+    return False
+
+def is_prime(n):
+    """
+    Checks the primality of n
+
+    Parameters
+    ----------
+    n : The number to check
+
+    Returns
+    -------
+    b: If n is prime, b is guaranteed to be True.
+    If n is not a prime, b has a chance of less than 1e-10 to be True
+    """
+    for _ in range(10):
+        if not miller_rabin(n):
+            return False
+    return True
+
+def generate_prime(digits):
+    for i in range(digits * 10):
+        n = randrange(10**(digits-1), 10**digits)
+        if is_prime(n):
+            return n
+    return None
+
+if __name__ == '__main__':
+    print(extended_gcd(5279, 797))
+    print(modular_exponent(123, 500, 100))
+    print(modular_exponent(123456, modular_exponent(7896543, 74365753, )))
+    counter = 0
+    rem = 0
+    num = 123456
+    hundreth_digit = 0
+    while(hundreth_digit!=5):
+        counter+=1
+        num=num*123456
+        rem = num % 1000
+        hundreth_digit = rem//100
+    print(counter)
+
---
+++ rsa_functions.py
@@ -0,0 +1,61 @@
+import number_theory_functions
+
+class RSA():
+    def __init__(self, public_key, private_key = None):
+        self.public_key = public_key
+        self.private_key = private_key
+
+    @staticmethod
+    def generate(digits = 10):
+        """
+        Creates an RSA encryption system object
+
+        Parameters
+        ----------
+        digits : The number of digits N should have
+
+        Returns
+        -------
+        RSA: The RSA system containing:
+        * The public key (N,e)
+        * The private key (N,d)
+        """
+        p = generate_prime(digits // 2 + 1)
+        q = generate_prime(digits// 2 + 1)
+        N = p * q
+        fi_N = (p-1) * (q-1)
+        for i in range(fi_N):
+            if extended_gcd(fi_N , i)[0] is 1:
+                public = i
+                break
+        private = modular_inverse(public,fi_N)
+        return RSA((N, public), (N, private))
+
+    def encrypt(self, m):
+        """
+        Encrypts the plaintext m using the RSA system
+
+        Parameters
+        ----------
+        m : The plaintext to encrypt
+
+        Returns
+        -------
+        c : The encrypted ciphertext
+        """
+        return modular_exponent(c, self.public_key[1], self.public_key[0])
+
+
+    def decrypt(self, c):
+        """
+        Decrypts the ciphertext c using the RSA system
+
+        Parameters
+        ----------
+        c : The ciphertext to decrypt
+
+        Returns
+        -------
+        m : The decrypted plaintext
+       """
+        return modular_exponent(c, self.private_key[1], self.private_key[0])
